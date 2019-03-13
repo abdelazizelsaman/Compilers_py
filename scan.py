47d2db8d897a22ef03d8ce8be1ce75dc6d4e6967
abdelazizelsaman <14p1064@eng.asu.edu.eg>
2019-03-13 17:04:40 +0200

    added DFA as a FSM

--- scan.py
+++ scan.py
@@ -8,9 +8,53 @@
 #       and classify each token to their role (token class).
 
 #       This scanner accepts input as a txt file & named "inputfile.txt"  
-#   `   & generates output as an array of dictionarys where each dict is 
+#       & generates output as an array of dictionarys where each dict is 
 #       labeled token in this format:
 #           {class: SyntaxKind.ConstKeyword, lexeme: ‘const’}
+
+
+from fysom import Fysom
+
+# TINY scanner DFA presented as FSM 
+DFA = Fysom(initial='START',
+            events=[('init', 'DONE', 'START'),
+                    ('whiteSpace',  'START',  '='),
+                    ('rb', 'START', 'INCOMMENT'),
+                    ('lb',  'INCOMMENT', 'START'),
+                    ('colon', 'START', 'ASSIGN'),
+                    ('eq', 'INASSIGN', 'DONE'),
+                    ('digit', 'INNUM', '='),
+                    ('digit', 'START', 'INNUM'),                    
+                    ('letter', 'START', 'INID'),
+                    ('letter', 'INID', '='),
+                    ('other', 'INCOMMENT', '='),
+                    ('other', 'INASSIGN', 'DONE'),
+                    ('other', 'INNUM', 'DONE'),
+                    ('other', 'INID', 'DONE'),
+                    ('other', 'START', 'DONE')])
+
+# function to control state transition for the scanner DFA
+def DFAstate (token):
+    for char in token:
+        if DFA.current == 'DONE':
+            DFA.init()
+        if char == ' ':
+                DFA.whiteSpace() if DFA.current == 'START' else DFA.other()
+        elif type(char) is int:
+            DFA.digit() if DFA.current == 'START' or DFA.current == 'INNUM' else DFA.other()
+        elif type(char) is str:
+            DFA.letter() if DFA.current == 'START' or DFA.current == 'INID' else DFA.other()
+        elif char == '{':
+            DFA.rb() if DFA.current == 'START' else DFA.other()
+        elif char == '}':
+            DFA.lb() if DFA.current == 'START' else DFA.other()
+        elif char == ':':
+            DFA.colon() if DFA.current == 'START' else DFA.other()
+        elif char == '=':
+            DFA.rb() if DFA.current == 'INASSIGN' else DFA.other()
+        else:
+            DFA.other()
+        stateLog.append(DFA.current) 
 
 
 #list of TINY language token classes
@@ -43,7 +87,10 @@ def other(lexeme):
 
 #function to identify each lexeme, label it & store it labeled in TokenList
 def identify(inputLines):
+    comment = False
     for lexeme in inputLines:
+        if comment is True and not('}' in lexeme) :
+            continue
         if lexeme in identifiers[0]:
             isSpecialSymbol(lexeme)
         elif ':' in lexeme:
@@ -55,7 +102,11 @@ def identify(inputLines):
                 isReservedWord(lexeme.replace(';',''))
             else:
                 other(lexeme.replace(';',''))
-            isSpecialSymbol(';')    
+            isSpecialSymbol(';')
+        elif '{' in lexeme and '}' not in lexeme  :
+            comment = True  
+        elif '}' in lexeme :
+                comment = False 
         else:
             other(lexeme)
     for t in range(len(tokenList)):
@@ -65,8 +116,12 @@ def identify(inputLines):
 tokenList = []
 #read input file
 file = open('inputfile.txt', 'r')
+sourceCode = file.read()
 #split tokens from input file, remove spaces
-inputSplitted = file.read().split()
+inputSplitted = sourceCode.split()
 #identify each token in the list & append it to TokenList
 identify(inputSplitted)
+#DFA State transition
+stateLog = []
+DFAstate(sourceCode)
 
