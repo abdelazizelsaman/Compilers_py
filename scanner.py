43b7f108509a4149c691c1944684a68aa000d0ec
abdelazizelsaman <14p1064@eng.asu.edu.eg>
2019-04-19 04:27:15 +0200

    added a separate module for the compiler's scanner, added another python file to test

---
+++ scanner.py
@@ -0,0 +1,72 @@
+
+#tokens array to be filled with each lexeme after being identified 
+__tokenList = []
+#list of TINY language token classes
+__identifiers = [['+', '-', '*', '/', '=', '<', '(', ')', ';', ':='],
+               ['if', 'then', 'else', 'end', 'repeat', 'until', 'read', 'write']]
+
+#function to label & store special symbols in TokenList
+def __isSpecialSymbol(lexeme):
+    __tokenList.append({"class" : "Special Symbol", "lexeme" : lexeme})
+
+#function to label & store reserved words in TokenList
+def __isReservedWord(lexeme):
+    __tokenList.append({"class" : "Reserved Word", "lexeme" : lexeme})
+
+#function to label & store numbers in TokenList
+def __isNum(lexeme):
+    __tokenList.append({"class" : "Number", "lexeme" : lexeme})
+
+#function to label & store identifiers in TokenList
+def __isIdentifier(lexeme):
+    __tokenList.append({"class" : "Identifier", "lexeme" : lexeme})
+
+#function to check whether the lexeme is number or identifier
+def __other(lexeme):
+    try:
+        val = int(lexeme)
+        __isNum(lexeme)
+    except ValueError:
+        __isIdentifier(lexeme)
+
+#function to identify each lexeme, label it & store it labeled in TokenList
+def __identify(inputSplitted):
+    comment = False
+    for lexeme in inputSplitted:
+        if comment is True and not('}' in lexeme) :
+            continue
+        if lexeme in __identifiers[0]:
+            __isSpecialSymbol(lexeme)       
+        elif lexeme in __identifiers[1]:
+            __isReservedWord(lexeme)
+        elif lexeme == '{':
+            comment = True  
+        elif '}' in lexeme :
+                comment = False 
+        #case where lexeme contains multiple tokens with no whitespaces in between
+        else:
+            temp = []
+            for x in __identifiers[0]:
+                if x in lexeme:
+                   temp = lexeme.split(x)
+                   temp.insert(1,x)                         
+            if len(temp) != 0:
+                __identify(temp)
+            elif '{' in lexeme:
+                temp = lexeme.split('{')    
+                temp.insert(1, '{')
+                __identify(temp)
+            elif '}' in lexeme:
+                temp = lexeme.split('}')
+                temp.insert(1, '}')
+                __identify(temp)   
+            elif lexeme != '' and lexeme != ' ':                              
+                __other(lexeme)
+
+def tokenize(sourceCode):
+    #split tokens from input file, remove spaces
+    inputSplitted = sourceCode.split()
+    __identify(inputSplitted)
+    return __tokenList
+
+    
