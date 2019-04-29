d103e14bcfdbdbe617139996f79ac80d221b26e0
abdelazizelsaman <14p1064@eng.asu.edu.eg>
2019-04-29 20:04:26 +0200

    parser script updated

--- parser.py
+++ parser.py
@@ -1,21 +1,51 @@
 import scanner
-from enum import Enum 
 index = 0
 TOKENCLASS = 'class'
 TOKENLEXEME = 'lexeme'
 IDENTIFIER = 'identifier'
 NUMBER = 'number'
+FLAG = True
+flag_rep = False
+PROGRAM = 'program'
+STMTSEQ = ' stmt_sequence'
+STATAMENT = 'statament'
+IFSTMT = 'if-stmt'
+REPEATSTMT = 'repeat-stmt'
+ASSIGNSTMT = 'assign-stmt'
+READSTMT = 'read-stmt'
+WRITESTMT = 'write-stmt'
+COMPARISONOP = 'comparison-op'
+SIMPLEEXP = 'simple-exp'
+ADDOP = 'addop'
+MULOP = 'mulop'
+IF = 'if'
+ELSE = 'else'
+EXP = 'exp'
+THEN = 'then'
+END = 'end'
+UNTIL = 'until'
+TERM = 'term'
+FOUND = ' is Found in '
+MISSING = ' is Missing in '
 
 tokenList_dummy = []
 file = open('tiny_sample_code.txt', 'r')
 tokenList_dummy = scanner.tokenize(file.read())
 print(tokenList_dummy)
 
+def printOutput(strg):
+    with open('parser_output.txt', 'w') as f:
+        f.write("%s\n" % strg)   
+
 def success():
-    print('Expected Input : ' + str(getToken(TOKENLEXEME)))
+    global FLAG
+    FLAG = True
+    print('Expected Input : '+getToken(TOKENLEXEME))
 
 def failed():
-     print('Syntax Error : ' + str(getToken(TOKENLEXEME)))
+    global FLAG
+    FLAG = False
+    print('Syntax Error : ')
 
 def nextToken():
     global index
@@ -40,9 +70,17 @@ def match(token, param):
    
 
 def program():
+    global index
+    ##
+    print(PROGRAM + FOUND)
+    ##
     stmtSeq()
+    if index < len(tokenList_dummy):
+        print('Syntax Error : Expected ; \n') 
+        program()
 
 def stmtSeq():
+    print(STMTSEQ + FOUND)
     statament()
     while getToken(TOKENLEXEME) == ';':
         match(';', TOKENLEXEME)
@@ -52,76 +90,101 @@ def stmtSeq():
 
 def statament():
     if 'if' == getToken(TOKENLEXEME):
+        print(STATAMENT + FOUND)
         ifstmt()
     elif 'repeat' == getToken(TOKENLEXEME):
+        print(STATAMENT + FOUND)
         repeatStmt()
     elif IDENTIFIER == getToken(TOKENCLASS):
+        print(STATAMENT + FOUND)
         assignstmt()
     elif 'read' == getToken(TOKENLEXEME):
+        print(STATAMENT + FOUND)
         readStmt()
     elif 'write' == getToken(TOKENLEXEME):
+        print(STATAMENT +FOUND)
         writeStmt()
     else:
-        match(False,TOKENCLASS)
-        statament()    
+        if index < len(tokenList_dummy) and not flag_rep:
+            match(False,TOKENCLASS)
+            print(STATAMENT + MISSING)
+            statament()    
 
 
 def ifstmt():
-    match('if', TOKENLEXEME)
+    print(IFSTMT+FOUND if match('if', TOKENLEXEME) else IFSTMT+MISSING+IFSTMT)
+    print(IF+FOUND)
     exp()
-    match('then', TOKENLEXEME)
+    print( THEN+FOUND if  match('then', TOKENLEXEME) else THEN+MISSING+IFSTMT)
     stmtSeq()
     if getToken(TOKENLEXEME) == 'else':
-        match('else', TOKENLEXEME)
+        print( ELSE+FOUND if match('else', TOKENLEXEME) else ELSE+MISSING+IFSTMT)
         stmtSeq()
-    match('end', TOKENLEXEME)    
+    print(END+FOUND if match('end', TOKENLEXEME) else END+MISSING+IFSTMT)    
 
 def repeatStmt():
-    match('repeat', TOKENLEXEME)
+    global flag_rep
+    flag_rep = True
+    print( REPEATSTMT+FOUND if match('repeat', TOKENLEXEME) else REPEATSTMT+MISSING+REPEATSTMT)
     stmtSeq()
-    match('until', TOKENLEXEME)
+    print(UNTIL+FOUND if match('until', TOKENLEXEME) else UNTIL+MISSING+REPEATSTMT)
     exp()
+    flag_rep = False
 
 def assignstmt():
+    print(ASSIGNSTMT+FOUND)
+    print(IDENTIFIER+FOUND)
     match(IDENTIFIER, TOKENCLASS)
-    match(':=', TOKENLEXEME)
-    exp()
+    if match(':=', TOKENLEXEME):
+        print(':='+FOUND)
+        exp()
+    else:
+        print('=:'+MISSING+ASSIGNSTMT)
+    
         
 
 def readStmt():
+    print(READSTMT+FOUND)
     match('read', TOKENLEXEME)
-    match(IDENTIFIER, TOKENCLASS)   
+    print(IDENTIFIER+FOUND if match(IDENTIFIER, TOKENCLASS) else IDENTIFIER+MISSING+READSTMT)   
         
 
 def writeStmt():
+    print(WRITESTMT+FOUND)
     match('write', TOKENLEXEME)
     exp()
 
 def exp():
+    print(EXP+FOUND)
     simpleExp()
     if '=' == getToken(TOKENLEXEME) or '<' == getToken(TOKENLEXEME):
         comparisonOp()
         simpleExp()
 
 def comparisonOp():
+    print(COMPARISONOP+FOUND)
     match(getToken(TOKENLEXEME),TOKENLEXEME)
 
 def simpleExp():
+    print(SIMPLEEXP+FOUND)
     term()
     while getToken(TOKENLEXEME) == '+' or getToken(TOKENLEXEME) == '-':
         addOp() 
         term()
 
 def addOp():
+    print(ADDOP+FOUND)
     match(getToken(TOKENLEXEME),TOKENLEXEME)
 
 def term():
+    print(TERM+FOUND)
     factor()
     while getToken(TOKENLEXEME) == '/' or getToken(TOKENLEXEME) == '*':
         mulOp()
         factor()
 
 def mulOp():
+    print(MULOP+FOUND)
     match(getToken(TOKENLEXEME),TOKENLEXEME)
 
 def factor():
@@ -130,11 +193,13 @@ def factor():
         exp()
         match(')',TOKENLEXEME)
     elif getToken(TOKENCLASS) == NUMBER:
-        match(getToken(TOKENCLASS), TOKENCLASS)
+        print(NUMBER+FOUND)
+        match(getToken(TOKENCLASS), TOKENCLASS) 
     elif getToken(TOKENCLASS) == IDENTIFIER:
         match(getToken(TOKENCLASS), TOKENCLASS)
     else:
-        match(False,TOKENCLASS) 
+        match(False,TOKENCLASS)
+        print('factor'+MISSING+TERM) 
         factor()   
 
 
